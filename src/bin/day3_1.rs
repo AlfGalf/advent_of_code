3dc506a389df5e81cba7497fb4a934b544487e94
Alfie Richards <alfie.richards@arm.com>
2023-12-03 18:48:12 +0000

    Day3

---
+++ src/bin/day3_1.rs
@@ -0,0 +1,52 @@
+use regex::Regex;
+use std::{fs, error::Error, usize};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string("inputs/day3.txt")?;
+    let mut lines = contents.lines();
+
+    let mut last_line: Option<&str> = None;
+    let mut current_line: Option<&str> = lines.next();
+    let mut next_line: Option<&str> = lines.next();
+
+    let mut result= 0;
+    let num_regex = Regex::new(r"\d+").unwrap();
+    let symbol_regex = Regex::new(r"[^\d\.]").unwrap();
+    while let Some(cur_line) = current_line {
+        for c in num_regex.captures_iter(cur_line) {
+            let capture = c.get(0).unwrap();
+            let start = capture.start();
+            let end = capture.end();
+
+            if 'cond: {
+                if let Some(last_line) = last_line {
+                    let section = &last_line[(start.max(1) - 1)..(end + 1).min(last_line.len())];
+                    if symbol_regex.is_match(section) {
+                        break 'cond true;
+                    }
+                }
+                let section = &cur_line[(start.max(1) - 1)..(end + 1).min(cur_line.len())];
+                if symbol_regex.is_match(section) {
+                    break 'cond true;
+                }
+                if let Some(next_line) = next_line {
+                    let section = &next_line[(start.max(1) - 1)..(end + 1).min(next_line.len())];
+                    if symbol_regex.is_match(section) {
+                        break 'cond true;
+                    }
+                }
+                false
+            } {
+                // println!("{}", capture.as_str());
+                result += capture.as_str().parse::<usize>().unwrap()
+            }
+        }
+
+        last_line = current_line;
+        current_line = next_line;
+        next_line = lines.next();
+    }
+    println!("{}", result);
+
+    Ok(())
+}
---
+++ src/bin/day3_2.rs
@@ -0,0 +1,55 @@
+use regex::Regex;
+use std::{fs, error::Error, usize};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string("inputs/day3.txt")?;
+    let mut lines = contents.lines();
+
+    let mut last_line: Option<&str> = None;
+    let mut current_line: Option<&str> = lines.next();
+    let mut next_line: Option<&str> = lines.next();
+
+    let mut result= 0;
+    let num_regex = Regex::new(r"\d+").unwrap();
+    let symbol_regex = Regex::new(r"(\d*)(\*)(\d*)").unwrap();
+    while let Some(cur_line) = current_line {
+        for c in symbol_regex.captures_iter(cur_line) {
+            let capture = c.get(2).unwrap();
+            let pos = capture.start();
+
+            let mut nums = vec![];
+
+            if let Some(last_line) = last_line {
+                for num in num_regex.captures_iter(last_line){
+                    let num_cap = num.get(0).unwrap();
+                    let num_int: usize = num_cap.as_str().parse().unwrap();
+                    if num_cap.end() >= pos && num_cap.start() <= pos + 1 {
+                        nums.push(num_int)
+                    }
+                }
+            }
+            if let Ok(left_num) = c.get(1).unwrap().as_str().parse::<usize>() { nums.push(left_num)}
+            if let Ok(right_num) = c.get(3).unwrap().as_str().parse::<usize>() { nums.push(right_num) }
+            if let Some(next_line) = next_line {
+                for num in num_regex.captures_iter(next_line){
+                    let num_cap = num.get(0).unwrap();
+                    let num_int: usize = num_cap.as_str().parse().unwrap();
+                    if num_cap.end() >= pos && num_cap.start() <= pos + 1 {
+                        nums.push(num_int)
+                    }
+                }
+            }
+
+            if nums.len() == 2 {
+                result += nums[0] * nums[1]
+            }
+        }
+
+        last_line = current_line;
+        current_line = next_line;
+        next_line = lines.next();
+    }
+    println!("{}", result);
+
+    Ok(())
+}
