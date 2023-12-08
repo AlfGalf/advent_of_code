fcb3670653c4392f429239b7b99c02fe65f14a3f
Alfie Richards <alfie.richards@arm.com>
2023-12-08 10:21:06 +0000

    Day8

---
+++ src/bin/day8_1.rs
@@ -0,0 +1,34 @@
+use regex::Regex;
+use std::{fs, error::Error, usize, collections::HashMap};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let binding = fs::read_to_string("inputs/day8.txt")?;
+
+    let first_line = binding.lines().next().unwrap().to_string();
+
+    let instructions= Regex::new(r"(?m)^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$").unwrap();
+
+    let instructions: HashMap<String, (String, String)> = instructions.captures_iter(&binding)
+        .map(|c| (c.get(1).unwrap().as_str().to_string(), (c.get(2).unwrap().as_str().to_string(), c.get(3).unwrap().as_str().to_string()))).collect();
+
+    let mut i = 0;
+    let mut cur = "AAA";
+    while cur != "ZZZ" {
+        for char in first_line.chars() {
+            i += 1;
+            println!("{cur}");
+            if cur == "ZZZ" { break; }
+            if char == 'L' {
+                cur = &instructions.get(cur).unwrap().0
+            } else if char == 'R' {
+                cur = &instructions.get(cur).unwrap().1
+            } else {
+                panic!("Unknown symbol")
+            }
+        }
+    }
+
+    println!("{i}");
+
+    Ok(())
+}
---
+++ src/bin/day8_2.rs
@@ -0,0 +1,40 @@
+use regex::Regex;
+use std::{fs, error::Error, collections::HashMap};
+use num::Integer;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let binding = fs::read_to_string("inputs/day8.txt")?;
+
+    let first_line = binding.lines().next().unwrap().to_string();
+
+    let instructions= Regex::new(r"(?m)^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$").unwrap();
+
+    let instructions: HashMap<String, (String, String)> = instructions.captures_iter(&binding)
+        .map(|c| (c.get(1).unwrap().as_str().to_string(), (c.get(2).unwrap().as_str().to_string(), c.get(3).unwrap().as_str().to_string()))).collect();
+
+    let cur: Vec<&String> = instructions.keys().filter(|p| p.ends_with("A")).collect();
+
+    let val: u128 = cur.into_iter().map(|s|{
+        let mut i: u128 = 0;
+        let mut cur = s;
+        'label: loop {
+            for char in first_line.chars() {
+                // println!("{cur:?}");
+                if cur.ends_with("Z") { break 'label; }
+                i += 1;
+                if char == 'L' {
+                    cur = &instructions.get(cur).unwrap().0
+                } else if char == 'R' {
+                    cur = &instructions.get(cur).unwrap().1
+                } else {
+                    panic!("Unknown symbol")
+                }
+            }
+        }
+        i
+    }).fold(1, |l, r| l.lcm(&r));
+
+    println!("{val}");
+
+    Ok(())
+}
