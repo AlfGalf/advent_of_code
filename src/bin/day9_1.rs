c83bdddd96a85fadac63801a95a9ef4d6ef28da4
Alfie Richards <alfie.richards@arm.com>
2023-12-09 09:35:45 +0000

    Day9

---
+++ src/bin/day9_1.rs
@@ -0,0 +1,35 @@
+use regex::Regex;
+use std::{fs, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let binding = fs::read_to_string("inputs/day9.txt")?;
+
+    let instructions= Regex::new(r"-?\d+").unwrap();
+
+    let instructions: Vec<Vec<isize>> = binding.lines().map(|l| instructions.captures_iter(l)
+        .map(|c| c.get(0).unwrap().as_str().parse().unwrap()).collect()).collect();
+
+    let val: isize = instructions.into_iter().map(|i| {
+        let mut arrs: Vec<Vec<isize>> = vec![i];
+        loop {
+            if arrs.last().unwrap().windows(2).all(|w| w[1] == w[0]) {break;}
+            arrs.push(arrs.last().unwrap().windows(2).map(|w| w[1] - w[0]).collect());
+        }
+
+        println!("{arrs:?}");
+
+        let val = *arrs.last().unwrap().first().unwrap();
+        arrs.last_mut().unwrap().push(val);
+
+        for i in (0..arrs.len()-1).rev() {
+            let val = arrs[i].last().unwrap() + arrs[i+1].last().unwrap();
+            arrs[i].push(val);
+        }
+        println!("{arrs:?}");
+        *arrs.first().unwrap().last().unwrap()
+    }).sum();
+
+    println!("{val}");
+
+    Ok(())
+}
